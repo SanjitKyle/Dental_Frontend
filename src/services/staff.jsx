ae8a292fad0dddf584110aeba2ac2d8ad13135ca
sanjitkyle <kyleinfotech14@gmail.com>
2026-09-17 19:12:07 +0530

    feat: connect Staff page to real API, add CRUD with toast notifications

---
+++ src/services/staff.jsx
@@ -0,0 +1,29 @@
+﻿import { axiosInstance } from "./axiosInstance";
+
+export const getStaff = async (token) => {
+  const response = await axiosInstance.get("/staff", {
+    headers: { authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
+
+export const createStaff = async (token, staffData) => {
+  const response = await axiosInstance.post("/staff", staffData, {
+    headers: { authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
+
+export const updateStaff = async (token, id, staffData) => {
+  const response = await axiosInstance.put(`/staff/${id}`, staffData, {
+    headers: { authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
+
+export const deleteStaff = async (token, id) => {
+  const response = await axiosInstance.delete(`/staff/${id}`, {
+    headers: { authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
